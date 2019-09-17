7fb038073e24fe5aa2cd8ca38d32bf9fc5da3f88
Rachel Zhang <zhangr@darden.virginia.edu>
2019-09-17 09:36:48 -0400

    added JWPlayer video integration

---
+++ V-0001.js
@@ -0,0 +1,11 @@
+function play() {  jwplayer("V-0001").setup({
+height: 270,
+width: 480,
+autostart: true,
+mute: false,
+control: true,
+repeat: false,
+sources : [{ file : "V-0001.mp4",
+label : "720"
+}]
+ }); }
